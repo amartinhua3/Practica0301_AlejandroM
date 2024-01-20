5d09e7b63087b65c43577efa23af545129445af3
amartinhua3 <amartinhua3@educacion.navarra.es>
2024-01-20 13:23:16 +0100

    ejercicio2

---
+++ Ejercicio2.py
@@ -0,0 +1,11 @@
+import csv
+
+
+def leer_csv50(lista50):
+   with open('50.csv', newline = '\n') as f:
+    datos = csv.reader(f, delimiter = ',')
+    
+   for i in range(len(lista50)):
+     
+               
+    
